83ad5915e6903d5f563597b7f5a93e6d17ef0d02
Yasuhiro <yasuhiro.corp@gmail.com>
2024-07-21 19:13:38 +0300

    Creating basic library

---
+++ include/some.h
@@ -0,0 +1,16 @@
+//
+// Created by Azazel on 21.07.2024.
+//
+
+#ifndef MPT_LIB_SOME_H
+#define MPT_LIB_SOME_H
+
+namespace sm
+{
+    namespace lbr
+    {
+        void print_something();
+    }
+}
+
+#endif //MPT_LIB_SOME_H
---
+++ src/some.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Azazel on 21.07.2024.
+//
+
+namespace sm
+{
+    namespace lbr
+    {
+        void print_something()
+        {
+            std::cout << "Hello" << std::endl;
+        }
+    }
+}
