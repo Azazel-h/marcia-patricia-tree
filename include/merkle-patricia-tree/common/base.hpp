bf140e300d60c666e9b65a5e0e83beaaf76de429
Yasuhiro <yasuhiro.corp@gmail.com>
2024-07-26 17:24:17 +0300

    <stdint.h> adding

--- include/merkle-patricia-tree/common/base.hpp
+++ include/merkle-patricia-tree/common/base.hpp
@@ -1,24 +1,6 @@
 #ifndef SILKWORM_BASE_HPP
 #define SILKWORM_BASE_HPP
-
 #ifdef __cplusplus
-/*
-   Copyright 2022 The Silkworm Authors
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
-#pragma once
 
 // The most common and basic macros, concepts, types, and constants.
 
@@ -44,35 +26,38 @@
 
 namespace silkworm {
 
-using namespace std::string_view_literals;
+    using namespace std::string_view_literals;
 
-template <class T>
-concept UnsignedIntegral = std::unsigned_integral<T> || std::same_as<T, intx::uint128> ||
-                           std::same_as<T, intx::uint256> || std::same_as<T, intx::uint512>;
+    template<class T>
+    concept UnsignedIntegral = std::unsigned_integral<T> || std::same_as<T, intx::uint128> ||
+                               std::same_as<T, intx::uint256> || std::same_as<T, intx::uint512>;
 
-using BlockNum = uint64_t;
-using BlockNumRange = std::pair<BlockNum, BlockNum>;
-using BlockTime = uint64_t;
+    using BlockNum = uint64_t;
+    using BlockNumRange = std::pair<BlockNum, BlockNum>;
+    using BlockTime = uint64_t;
 
-inline constexpr BlockNum kEarliestBlockNumber{0ul};
+    inline constexpr BlockNum kEarliestBlockNumber{0ul};
 
-inline constexpr size_t kAddressLength{20};
+    inline constexpr size_t kAddressLength{20};
 
-inline constexpr size_t kHashLength{32};
+    inline constexpr size_t kHashLength{32};
 
 // https://en.wikipedia.org/wiki/Binary_prefix
-inline constexpr uint64_t kKibi{1024};
-inline constexpr uint64_t kMebi{1024 * kKibi};
-inline constexpr uint64_t kGibi{1024 * kMebi};
-inline constexpr uint64_t kTebi{1024 * kGibi};
+    inline constexpr uint64_t kKibi{1024};
+    inline constexpr uint64_t kMebi{1024 * kKibi};
+    inline constexpr uint64_t kGibi{1024 * kMebi};
+    inline constexpr uint64_t kTebi{1024 * kGibi};
 
-inline constexpr uint64_t kGiga{1'000'000'000};   // = 10^9
-inline constexpr uint64_t kEther{kGiga * kGiga};  // = 10^18
+    inline constexpr uint64_t kGiga{1'000'000'000};   // = 10^9
+    inline constexpr uint64_t kEther{kGiga * kGiga};  // = 10^18
 
-constexpr uint64_t operator"" _Kibi(unsigned long long x) { return x * kKibi; }
-constexpr uint64_t operator"" _Mebi(unsigned long long x) { return x * kMebi; }
-constexpr uint64_t operator"" _Gibi(unsigned long long x) { return x * kGibi; }
-constexpr uint64_t operator"" _Tebi(unsigned long long x) { return x * kTebi; }
+    constexpr uint64_t operator "" _Kibi(unsigned long long x) { return x * kKibi; }
+
+    constexpr uint64_t operator "" _Mebi(unsigned long long x) { return x * kMebi; }
+
+    constexpr uint64_t operator "" _Gibi(unsigned long long x) { return x * kGibi; }
+
+    constexpr uint64_t operator "" _Tebi(unsigned long long x) { return x * kTebi; }
 
 }  // namespace silkworm
 
@@ -82,6 +67,7 @@ constexpr uint64_t operator"" _Tebi(unsigned long long x) { return x * kTebi; }
 #ifdef __cplusplus
 extern "C" {
 #endif
+#include <stdint.h>
 
 typedef uint64_t BlockNum;
 typedef uint64_t BlockTime;
