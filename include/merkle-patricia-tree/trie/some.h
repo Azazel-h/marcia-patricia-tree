f88375cccb525216087e6471c72e0ff7eef998b4
Yasuhiro <yasuhiro.corp@gmail.com>
2024-07-26 17:12:36 +0300

    Trying to make a some.h wrapper

--- include/merkle-patricia-tree/trie/some.h
+++ include/merkle-patricia-tree/trie/some.h
@@ -1,16 +1,28 @@
 //
 // Created by Azazel on 21.07.2024.
 //
+#ifndef SILKWORM_TRIE_SOME_HPP
+#define SILKWORM_TRIE_SOME_HPP
 
 #ifdef __cplusplus
-#pragma once
 
 #include <iomanip>
 #include <iostream>
 
 
 namespace sm::lbr {
-    extern "C" void print_something();
+    void print_something();
 }
 #endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void print_something() { sm::lbr::print_something(); } ;
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SILKWORM_TRIE_SOME_HPP
 
